320e42a0330ec920a975b13ac7ae5771bb5cb973
farah-massuh <farahmassuh.dataanalyst@gmail.com>
2025-04-03 16:53:17 -0400

    business questions

--- sql_files/3_table_populate.sql
+++ sql_files/3_table_populate.sql
@@ -12,3 +12,16 @@
 
 -- warranty
 \copy warranty FROM '/Users/farahmassuh/Desktop/apple_sql/csv_files/warranty.csv' WITH (FORMAT csv, HEADER true, DELIMITER ',', ENCODING 'UTF8');
+
+-- EDA
+SELECT DISTINCT repair_status FROM warranty;
+SELECT COUNT(*) FROM sales;
+
+-- Improving Query Performance
+CREATE INDEX sales_product_id ON sales(product_id);
+CREATE INDEX sales_store_id ON sales(store_id);
+CREATE INDEX sales_sale_date ON sales(sale_date);
+
+EXPLAIN ANALYZE
+SELECT * FROM sales
+WHERE product_id = 'P-44'
